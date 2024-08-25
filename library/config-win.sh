abb88b917fa8d60de86e3ea1c77491a18d8c8d07
starg2 <75976488+starg2@users.noreply.github.com>
2024-08-25 11:07:44 +0900

    Update host toolchains

--- library/config-win.sh
+++ library/config-win.sh
@@ -35,9 +35,9 @@
 
 # **************************************************************************
 
-readonly HOST_MINGW_VERSION=12.2.0
-readonly HOST_MINGW_RT_VERSION=10
-readonly HOST_MINGW_BUILD_REV=2
+readonly HOST_MINGW_VERSION=14.2.0
+readonly HOST_MINGW_RT_VERSION=12
+readonly HOST_MINGW_BUILD_REV=0
 readonly i686_HOST_MINGW_PATH_URL="https://github.com/niXman/mingw-builds-binaries/releases/download/$HOST_MINGW_VERSION-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV/i686-$HOST_MINGW_VERSION-release-posix-{exceptions}-{msvcrt}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
 readonly x86_64_HOST_MINGW_PATH_URL="https://github.com/niXman/mingw-builds-binaries/releases/download/$HOST_MINGW_VERSION-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV/x86_64-$HOST_MINGW_VERSION-release-posix-{exceptions}-{msvcrt}-rt_v$HOST_MINGW_RT_VERSION-rev$HOST_MINGW_BUILD_REV.7z"
 
